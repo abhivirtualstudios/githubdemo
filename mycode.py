2f7fe6cc6cdbb23375bb3a5f6695c70aa767417b
abhivirtualstudios <abhivirtualstudios@gmail.com>
2022-06-06 06:37:03 +0530

    Update mycode.py
    Making more changes to test fetch and merge

--- mycode.py
+++ mycode.py
@@ -3,4 +3,5 @@ import mymodule
 print("my first line")
 print("my second line")
 print("my most latest third line")
-print('changing directly from github)
+print("changing directly from github")
+print("changing from github to check fetch and merge")
